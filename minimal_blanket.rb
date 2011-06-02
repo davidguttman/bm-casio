317dba8ac4a08bb5f88ed7ad595e3447026b917b
David Guttman <david@davidguttman.com>
2011-06-01 21:55:46 -0700

    osc basics working

--- minimal_blanket.rb
+++ minimal_blanket.rb
@@ -6,16 +6,51 @@ ENV['GEM_HOME'] = '/Users/dguttman/.rvm/gems/jruby-1.6.1'
 ENV['GEM_PATH'] = '/Users/dguttman/.rvm/gems/jruby-1.6.1:/Users/dguttman/.rvm/gems/jruby-1.6.1@global'
 
 class MinimalBlanket < Processing::App
-  
 
   load_libraries :trig
 
+  include Trig
+
   def setup
-    p polar(200, 4302)
+    @osc = OSCHelper.new
   end
   
   def draw
+    if frame_count % 60 == 0
+      @osc.send('/hello', "sup")
+    end
+  end
   
+end
+
+class OSCHelper
+  require 'osc-ruby'
+  require 'osc-ruby/em_server'
+  include OSC
+  
+  attr_reader :server, :client
+  
+  def initialize(opts={})
+    puts "Starting OSC server and client ..."
+    @server = EMServer.new 9090
+    @client = Client.new 'localhost', 9090
+    
+    Thread.new do
+      @server.run
+    end
+    
+    set_patterns
+  end
+  
+  def send(address, message)
+    m = Message.new(address, message)
+    @client.send(m)
+  end
+  
+  def set_patterns
+    @server.add_method '/hello' do |message|
+      p message
+    end
   end
   
 end
